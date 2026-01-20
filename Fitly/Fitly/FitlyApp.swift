196df3ecc7db109fa9950f9905429e813e8d8d5c
Hamzeh <Hamzehalgahmi@gmail.com>
2026-01-19 21:41:01 -0500

    Baseline: new Xcode project runs

---
+++ Fitly/Fitly/FitlyApp.swift
@@ -0,0 +1,17 @@
+//
+//  FitlyApp.swift
+//  Fitly
+//
+//  Created by Amal Algirsh on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct FitlyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
